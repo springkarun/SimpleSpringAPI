8f96be6689ef089ce5961b2cd42559bea1bb4711
pata02525 <hata02525@gmail.com>
2018-06-09 19:12:29 +0530

    Spring simple API

---
+++ src/main/java/com/spring/SpringDemo/MainApplicationClass.java
@@ -0,0 +1,12 @@
+package com.spring.SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplicationClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplicationClass.class, args);
+    }
+}
